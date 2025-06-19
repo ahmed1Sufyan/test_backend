38ea78fcc6b6f87f440054ccc7496fc5fd62b59c
sufyan ahmed <csc21s135@gmail.com>
2025-06-20 02:23:59 +0500

    add route

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.use("/health",(req,res)=>{
         msg : "Everything is ok"
     })
 })
+app.use("/",(req,res)=>{
+    res.json({
+        msg : "Deployment worked"
+    })
+})
 
 app.listen(7000,()=>{
     console.log("server is listening on port 7000");
